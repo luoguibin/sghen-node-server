24734a4b177544f044119fba69eb8475b1bcdfcd
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-06-09 11:24:07 +0800

    [+]测试新增API

--- test/index.js
+++ test/index.js
@@ -1,20 +1,34 @@
 const request = require('request')
+const auth = require('../core/auth')
 
 const outputFunc = function (error, response, body) {
+  // console.log(error, response, body)
   if (!error && +response.statusCode === 200) {
     console.log(body)
   } else {
     console.log(error)
   }
 }
+// const host = 'http://localhost:8282'
+// const baseUrl = host
+const host = 'https://www.sghen.cn'
+const baseUrl = host + '/napi'
+
+const token = auth.newToken({ userId: 16405, level: 9 })
+auth.verify(token, data => {
+  console.log('auth.verify', data)
+})
 
 request.post({
-  url: 'http://localhost:8087/auth/api-center/update',
+  url: `${baseUrl}/auth/api-center/update`,
+  headers: {
+    Authorization: token
+  },
   form: {
-    id: 3,
+    id: 1,
     name: '查询最新的10条API数据',
     comment: '',
-    content: JSON.stringify([{ sql: 'SELECT d.*, u.user_name, u.avatar FROM dynamic_api2 d LEFT JOIN user u ON d.user_id=u.id LIMIT 10' }]),
+    content: JSON.stringify([{ key: 'data', sql: 'SELECT d.*, u.user_name, u.avatar FROM dynamic_api2 d LEFT JOIN user u ON d.user_id=u.id LIMIT 10' }]),
     status: 1,
     userId: 16405,
     suffixPath: 'list'
@@ -26,7 +40,7 @@ request.post({
 //   { key: 'list', sql: 'SELECT id, user_name AS userName, avatar FROM user LIMIT ${limit} OFFSET ${offset}' }
 // ])
 // request.post({
-//   url: 'http://localhost:8087/auth/api-center/update',
+//   url: `${baseUrl}/auth/api-center/update`,
 //   form: {
 //     id: 1,
 //     name: '查询最新的10条用户数据',
