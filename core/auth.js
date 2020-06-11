f19a4fe1c18acf1be587f0685a2636a5346b6cf0
i_gbluo(罗桂彬) <i_gbluo@tencent.com>
2020-06-11 09:10:54 +0800

    [*]认证

--- core/auth.js
+++ core/auth.js
@@ -2,14 +2,18 @@ const JWT = require('jsonwebtoken')
 const { auth: authConfig } = require('../config')
 
 const newToken = function (data = {}) {
+  const { userId, userName, uLevel } = data
+  if (!userId) {
+    return ''
+  }
   const time = Math.floor(Date.now() / 1000)
   return JWT.sign({
     exp: time + authConfig.expDuration,
     iat: time,
     data: {
-      userId: '',
-      userName: '',
-      uLevel: 0
+      userId,
+      userName,
+      uLevel
     }
   }, authConfig.SECRET_KEY)
 }
