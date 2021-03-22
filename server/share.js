be33e2ddd14686833c34cf5088a7cce385391131
luoguibin <945654877@qq.com>
2021-03-22 09:51:05 +0800

    [+]分享模块限制

--- server/share.js
+++ server/share.js
@@ -3,6 +3,10 @@ const db = require('../core/db')
 const { GetResponseData, CONST_NUM } = require('./base')
 const timeUtil = require('../utils/time')
 
+const SHARE_MODULE_MAP = {
+  resume: true,
+}
+
 const queryData = function (tableName, id, userId) {
   return new Promise(function (resolve, reject) {
     const sql = `SELECT * FROM ${tableName} WHERE id=? AND user_id=?`
@@ -59,6 +63,10 @@ const init = function (app) {
   })
   app.post('/auth/share/create', function (req, res) {
     const { shareId, shareModule, shareDuration } = req.body || {}
+    if (!SHARE_MODULE_MAP[shareModule]) {
+      res.send(GetResponseData(CONST_NUM.ERROR, '禁止分享当前模块'))
+      return
+    }
     const code = auth.newShareToken(req.auth.userId, shareId, shareModule, +shareDuration)
     res.send(GetResponseData({ code, currentTime: timeUtil.getTime(), shareDuration }))
   })
